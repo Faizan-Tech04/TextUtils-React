47eeeb79a18196583b81b8c8ab81f003cc438606
Faizan Chougule <faizanchougle03@gmail.com>
2026-05-29 18:07:42 +0530

    Final Update

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar(props) {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       <li className="nav-item active">
-        <Link className="nav-link active" to="/">Home</Link>
+        <Link className="nav-link" to="/">Home</Link>
       </li>
      {/* <a className="nav-item" aria-current="page" href="#">Home </a> */}
 
